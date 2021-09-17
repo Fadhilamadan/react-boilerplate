fde83bb7e685ef8be646d0601bff052a42ff6886
Fadhil Amadan <git@fadhilamadan.com>
2021-09-17 15:47:07 +0700

    feat(post): add component post index

---
+++ src/components/post/index.js
@@ -0,0 +1,42 @@
+import React, { useLayoutEffect } from 'react';
+
+import { getPost } from '../../redux/middleware/post/post.middleware';
+
+import PostCard from './card';
+import PostCardLoader from './cardLoader';
+
+import { connect } from 'react-redux';
+
+const Post = (props) => {
+  useLayoutEffect(() => {
+    props.onGetPost();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {!props.getPostReducer.isLoading ? (
+        <PostCardLoader count={6} />
+      ) : (
+        props.getPostReducer.item.map((item) => {
+          return <PostCard body={item.body} title={item.title} />;
+        })
+      )}
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    getPostReducer: state.getPostReducer,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onGetPost: () => {
+      return dispatch(getPost);
+    },
+  };
+};
+
+export default connect(mapState, mapDispatchToProps)(Post);
