ee98272eb4735d66899939c048e19496ed75abfe
Fadhil Amadan <git@fadhilamadan.com>
2021-09-17 15:44:39 +0700

    feat(post): add component post card loader

---
+++ src/components/post/cardLoader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { countLoader } from '../../utils/func/array';
+
+import ContentLoader from 'react-content-loader';
+
+const PostCardLoader = (props) => {
+  return countLoader(props.count).map(() => {
+    return (
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <ContentLoader
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb"
+          height={120}
+          speed={2}
+          viewBox="0 0 400 120"
+          width={400}
+          {...props}
+        >
+          <rect height="20" rx="3" ry="3" width="150" x="0" y="0" />
+          <rect height="40" rx="3" ry="3" width="350" x="0" y="25" />
+          <rect height="20" rx="3" ry="3" width="80" x="0" y="100" />
+        </ContentLoader>
+      </div>
+    );
+  });
+};
+
+export default PostCardLoader;
